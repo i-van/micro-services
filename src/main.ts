630ad7cf69cc776919b1ff9766f2061e43d1e70f
Ivan Nosov <i.k.nosov@gmail.com>
2023-03-23 19:24:13 -0700

    feat(main): implement graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,21 @@ const bootstrap = async () => {
         : console.log(`"${moduleName}" failed`);
     }
   }
+
+  process.on('SIGTERM', () => {
+    setTimeout(() => process.exit(1), 1000);
+
+    modules.forEach(async (moduleName) => {
+      try {
+        const module = require(`${__dirname}/${moduleName}`);
+        await module.stop();
+        console.log(`"${moduleName}" stopped`);
+      } catch (err) {
+        err.code === 'MODULE_NOT_FOUND'
+          ? console.log(`"${moduleName}" not found`)
+          : console.log(`"${moduleName}" failed`);
+      }
+    });
+  });
 };
 bootstrap();
